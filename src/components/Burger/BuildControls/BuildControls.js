ad3eeee32d70f7d2915ebc9086dd96cb3fc3ebcd
Sten Scheifel <sten.scheifel@gmail.com>
2021-04-12 21:43:44 +0300

    dynamic controls added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,11 +9,17 @@ const controls = [
   { label: "Meat", type: "meat" },
 ];
 
-const BuildControls = () => {
+const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
       {controls.map((ctrl) => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          added={() => props.ingredientAdded(ctrl.type)}
+          key={ctrl.label}
+          label={ctrl.label}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
       ))}
     </div>
   );
